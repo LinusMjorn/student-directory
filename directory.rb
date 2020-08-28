c8c18e688b4a34a5b1ca9800d4ccb86443d19d06
Linus Mjornstedt <carl.mjornstedt@gmail.com>
2020-08-28 12:12:22 +0100

    interactive menu added

--- directory.rb
+++ directory.rb
@@ -27,7 +27,25 @@ def print_footer_names(names)
   puts "Overall, we have #{names.count} great students"
 end
 
-students = input_students
-print_header
-print(students)
-print_footer_names(students)
+def interactive_menu
+  students = []
+  loop do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    selection = gets.chomp
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer_names(students)
+    when "9" #this will cause the program to terminate
+    else
+      puts "I dont know what you mean, try again"
+    end
+  end
+end
+
+interactive_menu
